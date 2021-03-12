c1c9f2389ee03da5415eada0c428bbd08b0a9fac
YousefAlhamed <yousefalhamed7@gmail.com>
2021-03-12 06:48:01 +0300

    Adding and Filtering Category

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,25 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, FlatList } from "react-native";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
 import { Container, Icon, Input, Item, Header } from "native-base";
 
 import ProductList from "./ProductList";
 import SearchedProducts from "./SearchedProducts";
+import Banner from "../../Shared/Banner";
+import CategoryFilter from "./CategoryFilter";
 
-let data = require("../../assets/data/products.json");
+const data = require("../../assets/data/products.json");
+const productCategories = require("../../assets/data/categories.json");
+
+var { height } = Dimensions.get("window");
 
 function ProductContainer(props) {
   const [products, setProducts] = useState([]);
   const [productsFiltered, setProductsFiltered] = useState([]);
   const [focus, setFocus] = useState();
+  const [categories, setCategories] = useState([]);
+  const [productsCtg, setProductsCtg] = useState([]);
+  const [active, setActive] = useState();
+  const [initialState, setInitialState] = useState([]);
 
   useEffect(() => {
     setProducts(data);
     setProductsFiltered(data);
     setFocus(false);
+    setCategories(productCategories);
+    setProductsCtg(data);
+    setActive(-1);
+    setInitialState(data);
     return () => {
       setProducts([]);
       setProductsFiltered([]);
       setFocus();
+      setCategories([]);
+      setActive();
+      setInitialState([]);
     };
   }, []);
 
@@ -35,6 +51,20 @@ function ProductContainer(props) {
 
   const onBlur = () => {
     setFocus(false);
+  };
+
+  // Categories
+  const changeCtg = (ctg) => {
+    {
+      ctg === "all"
+        ? [setProductsCtg(initialState), setActive(true)]
+        : [
+            setProductsCtg(
+              products.filter((i) => i.category._id === ctg),
+              setActive(true)
+            ),
+          ];
+    }
   };
 
   return (
@@ -53,19 +83,33 @@ function ProductContainer(props) {
       {focus == true ? (
         <SearchedProducts productsFiltered={productsFiltered} />
       ) : (
-        <View style={styles.container}>
-          <Text>Product items</Text>
-          <View style={styles.listContainer}>
-            <FlatList
-              data={products}
-              numColumns={2}
-              keyExtractor={(item) => item.brand}
-              renderItem={({ item }) => (
-                <ProductList key={item.brand} item={item} />
-              )}
-            />
+        <ScrollView>
+          <View>
+            <View>
+              <Banner />
+            </View>
+            <View>
+              <CategoryFilter
+                categories={categories}
+                categoryFilter={changeCtg}
+                productsCtg={productsCtg}
+                active={active}
+                setActive={setActive}
+              />
+            </View>
+            {productsCtg.length > 0 ? (
+              <View style={styles.listContainer}>
+                {productsCtg.map((item) => {
+                  return <ProductList key={item.name} item={item} />;
+                })}
+              </View>
+            ) : (
+              <View style={[styles.center, { height: height / 2 }]}>
+                <Text>No products found</Text>
+              </View>
+            )}
           </View>
-        </View>
+        </ScrollView>
       )}
     </Container>
   );
@@ -77,11 +121,16 @@ const styles = StyleSheet.create({
     backgroundColor: "gainsboro",
   },
   listContainer: {
+    height: height + 120,
     flex: 1,
-    width: "100%",
     flexDirection: "row",
     alignItems: "flex-start",
     flexWrap: "wrap",
+    backgroundColor: "gainsboro",
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
---
+++ Shared/Banner.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
+import Swiper from "react-native-swiper";
+
+var { width } = Dimensions.get("window");
+
+function Banner(props) {
+  const [bannerData, setBannerData] = useState([]);
+
+  useEffect(() => {
+    setBannerData([
+      "https://images.vexels.com/media/users/3/126443/preview2/ff9af1e1edfa2c4a46c43b0c2040ce52-macbook-pro-touch-bar-banner.jpg",
+      "https://pbs.twimg.com/media/D7P_yLdX4AAvJWO.jpg",
+      "https://www.yardproduct.com/blog/wp-content/uploads/2016/01/gardening-banner.jpg",
+    ]);
+    return () => {
+      setBannerData([]);
+    };
+  }, []);
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.swiper}>
+          <Swiper
+            style={{ height: width / 2 }}
+            showsButtons={false}
+            autoplay={true}
+            autoplayTimeout={4}
+          >
+            {bannerData.map((item) => {
+              return (
+                <Image
+                  key={item}
+                  style={styles.imageBanner}
+                  resizeMode="contain"
+                  source={{ uri: item }}
+                />
+              );
+            })}
+          </Swiper>
+          <View style={{ height: 20 }}></View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "gainsboro",
+  },
+  swiper: {
+    width: width,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  imageBanner: {
+    height: width / 2,
+    width: width - 40,
+    borderRadius: 10,
+    marginHorizontal: 20,
+  },
+});
+
+export default Banner;
